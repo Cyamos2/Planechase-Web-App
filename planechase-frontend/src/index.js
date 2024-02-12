255e2c239043349e90aa662bc267aa8d6b07ae12
Chris Amos <cyamos2@yahoo.com>
2024-02-12 12:17:32 -0500

    revised structure

--- planechase-frontend/src/index.js
+++ planechase-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App'; // Adjust the import path as needed
 
 ReactDOM.render(
   <React.StrictMode>
