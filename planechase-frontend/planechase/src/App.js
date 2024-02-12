9e7a8087a0472c6d32f0100325201f261b8144b1
Chris Amos <cyamos2@yahoo.com>
2024-02-12 10:34:23 -0500

    started

---
+++ planechase-frontend/planechase/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Planechase Web App</h1>
+      </header>
+      <main className="app-main">
+        {/* Add your components and content here */}
+        <p>Welcome to the Planechase Web App!</p>
+      </main>
+      <footer className="app-footer">
+        <p>Developed by Your Name</p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
